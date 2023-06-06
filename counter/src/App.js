29c533247d2fcb4d7b53ed8c797af50fb4f5e3ab
mametchi <tinyaqui@gmail.com>
2023-06-06 20:04:51 +0300

    props length test

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  var viesti = "";
+
+  //asetetaan viestimuuttuja propsin pituudesta riippuen
+  //if-else kuten javascriptissa
+  if (props.message.length < 10) {
+    viesti = props.message;
+  } else {
+    viesti = "Too long";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {viesti}
     </div>
   );
 }
 
 export default App;
+
